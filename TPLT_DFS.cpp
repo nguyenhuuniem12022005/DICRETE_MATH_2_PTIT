5127270c755982a509b948533c1950c1eb63ced1
NiemHuu <88307269+nguyenhuuniem12022005@users.noreply.github.com>
2025-06-23 09:37:29 +0700

    Update TPLT_DFS.cpp

--- TPLT_DFS.cpp
+++ TPLT_DFS.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 bool visited[100];
 int n;
-int TPLT_dfs(int a[100][100])
-{
-    int count = 0;
-    for (int i = 1; i <= n; i++) {
-        if (!visited[i]) {
-            count++;
-            stack<int> s;
-            s.push(i);
-            visited[i] = true;
-            while (!s.empty()) {
-                int v = s.top();
-                s.pop();
-                for (int j = 1; j <= n; j++) {
-                    if (a[v][j] == 1 && !visited[j]) {
-                        visited[j] = true;
-                        s.push(j);
-                    }
-                }
-            }
+
+void DFS(int u, int a[100][100]) {
+    visited[u] = true;
+    for(int i = 1; i <= n; i++) {
+        if(a[u][i] == 1 && !visited[i]) {
+            DFS(i, a); // truyền mảng a tiếp tục
         }
     }
-    return count;
 }
+
+int ConCom_DFS(int a[100][100]) {
+    int cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            cnt += 1;
+            DFS(i, a); // truyền mảng a vào DFS
+        }
+    }
+    return cnt;
+}
+
 int main() {
-    cin >> n;
     int a[100][100];
+    cin >> n;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> a[i][j];
         }
     }
+
     memset(visited, false, sizeof(visited));
-    int components = TPLT_dfs(a);
+    int components = ConCom_DFS(a);
     cout << "Number of connected components: " << components << endl;
+
     return 0;
 }
